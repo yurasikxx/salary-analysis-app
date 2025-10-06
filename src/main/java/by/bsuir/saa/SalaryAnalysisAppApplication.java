abf136d55a385bf07498de4619e52a564c28df03
Yury Tkachyk <yurasikxx@gmail.com>
2025-10-06 18:38:31 +0300

    Initital commit: Spring Boot project setup

---
+++ src/main/java/by/bsuir/saa/SalaryAnalysisAppApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.saa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryAnalysisAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryAnalysisAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/bsuir/saa/SalaryAnalysisAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.saa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryAnalysisAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
